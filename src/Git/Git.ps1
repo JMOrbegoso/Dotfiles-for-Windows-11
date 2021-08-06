55ff454d959db99b4bb2716d753b946a83007a6c
JMOrbegoso <dev@jmorbegoso.com>
2021-08-06 08:23:42 -0500

    Add Git.ps1

---
+++ src/Git/Git.ps1
@@ -0,0 +1,11 @@
+function Set-Git-Configuration
+{
+  Write-Host "Configuring Git:" -ForegroundColor "Green";
+  git config --global init.defaultBranch "main";
+  git config --global user.name $Config.GitUserName;
+  git config --global user.email $Config.GitUserEmail;
+  Write-Host "Git was successfully configured." -ForegroundColor "Green";
+}
+
+choco install -y "git" --params "/NoAutoCrlf /WindowsTerminal /NoShellIntegration /SChannel";
+Set-Git-Configuration;
