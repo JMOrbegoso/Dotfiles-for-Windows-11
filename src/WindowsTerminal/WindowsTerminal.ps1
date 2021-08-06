cc0fc55f28e6ea96c7e461c8f3a642150c443998
JMOrbegoso <dev@jmorbegoso.com>
2021-08-05 20:10:43 -0500

    Update WindowsTerminal.ps1 Add function  Set-OhMyPosh-Theme.

--- src/WindowsTerminal/WindowsTerminal.ps1
+++ src/WindowsTerminal/WindowsTerminal.ps1
@@ -1,1 +1,13 @@
+function Set-OhMyPosh-Theme
+{
+  $DotfilesOhMyPoshThemePath = Join-Path -Path $DotfilesWorkFolder -ChildPath "WindowsTerminal" | Join-Path -ChildPath ".oh-my-posh-custom-theme.omp.json";
+
+  Write-Host "Coping Oh My Posh custom theme:" -ForegroundColor "Green";
+
+  Copy-Item $DotfilesOhMyPoshThemePath -Destination $env:UserProfile;
+
+  Write-Host "Oh My Posh theme was successfully created." -ForegroundColor "Green";
+}
+
 Install-Module -Name "oh-my-posh";
+Set-OhMyPosh-Theme;
