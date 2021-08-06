2334a6f22fb4b8aab8465fc3975dfc393f9c55f4
JMOrbegoso <dev@jmorbegoso.com>
2021-08-05 20:15:50 -0500

    Update WindowsTerminal.ps1 Add function Set-PowerShell-Profile.

--- src/WindowsTerminal/WindowsTerminal.ps1
+++ src/WindowsTerminal/WindowsTerminal.ps1
@@ -9,5 +9,25 @@ function Set-OhMyPosh-Theme
   Write-Host "Oh My Posh theme was successfully created." -ForegroundColor "Green";
 }
 
+function Set-PowerShell-Profile
+{
+  $DotfilesWindowsTerminalProfilePath = Join-Path -Path $DotfilesWorkFolder -ChildPath "WindowsTerminal" | Join-Path -ChildPath "Microsoft.PowerShell_profile.ps1";
+
+  if (-not (Test-Path -Path $Profile))
+  {
+    Write-Host "Creating empty PowerShell profile:" -ForegroundColor "Green";
+    New-Item -Path $Profile -ItemType "file" -Force;
+  }
+  
+  Write-Host "Coping PowerShell profile:" -ForegroundColor "Green";
+  Copy-Item $DotfilesWindowsTerminalProfilePath -Destination $Profile;
+  
+  Write-Host "Activating PowerShell profile:" -ForegroundColor "Green";
+  . $Profile;
+
+  Write-Host "PowerShell profile was successfully created." -ForegroundColor "Green";
+}
+
 Install-Module -Name "oh-my-posh";
 Set-OhMyPosh-Theme;
+Set-PowerShell-Profile;
