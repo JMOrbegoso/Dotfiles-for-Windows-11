8d2355359f2a0ae13e4bbb462c75fd263f1ed588
JMOrbegoso <dev@jmorbegoso.com>
2021-08-06 17:37:28 -0500

    Update Notepad++.ps1 Add function Set-NotepadPlusPlus-Extensions.

--- src/Notepad++/Notepad++.ps1
+++ src/Notepad++/Notepad++.ps1
@@ -10,12 +10,27 @@ function Set-NotepadPlusPlus-Configuration
   else
   {
     Write-Host "Configuring Notepad++:" -ForegroundColor "Green";
-
+    
     Get-ChildItem -Path "${DotfilesNotepadPlusPlusConfigurationPath}\*" -Include *.xml -Recurse | Copy-Item -Destination $NotepadPlusPlusConfigurationPath -Force;
 
     Write-Host "Notepad++ was successfully configured." -ForegroundColor "Green";
   }
 }
 
+function Set-NotepadPlusPlus-Extensions
+{
+  $NotepadPlusPlusExtensions = ".txt", ".xml";
+  
+  Write-Host "Configuring Notepad++ as Microsoft Windows default program:" -ForegroundColor "Green";
+
+  foreach($Extension in $NotepadPlusPlusExtensions)
+  {
+    Set-FTA "Applications\notepad++.exe" $Extension;
+  };
+
+  Write-Host "Notepad++ was successfully configured as Microsoft Windows default program." -ForegroundColor "Green";
+}
+
 choco install -y "notepadplusplus";
 Set-NotepadPlusPlus-Configuration;
+Set-NotepadPlusPlus-Extensions;
