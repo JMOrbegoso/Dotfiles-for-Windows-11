38cd17a74b414f99a696931bf3f85cdd3e4413dc
JMOrbegoso <dev@jmorbegoso.com>
2021-08-07 13:34:59 -0500

    Add PSRepository.ps1 in Helpers

---
+++ src/Helpers/PSRepository.ps1
@@ -0,0 +1,27 @@
+function Get-PSRepository-Trusted-Status
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Position = 0, Mandatory = $TRUE)]
+    [string]
+    $PSRepositoryName
+  )
+
+  try
+  {
+    if (-not (Get-PSRepository -Name $PSRepositoryName -ErrorAction SilentlyContinue))
+    {
+      return $FALSE;
+    }
+
+    if ((Get-PSRepository -Name $PSRepositoryName).InstallationPolicy -eq "Trusted")
+    {
+      return $TRUE;
+    }
+    return $FALSE;
+  }
+  catch [Exception]
+  {
+    return $FALSE;
+  }
+}
