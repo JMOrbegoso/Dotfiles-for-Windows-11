6d3140ae17e82e885edb47e4795823dc5f63faeb
JMOrbegoso <dev@jmorbegoso.com>
2021-08-06 19:40:59 -0500

    Add Clean.ps1 in Helpers

---
+++ src/Helpers/Clean.ps1
@@ -0,0 +1,12 @@
+function Remove-Desktop-Shortcuts
+{
+  $UserDesktopPath = [Environment]::GetFolderPath("Desktop");
+  $PublicDesktopPath = "${env:Public}\Desktop";
+
+  Write-Host "Deleting shorcuts in desktop:" -ForegroundColor "Green";
+
+  Get-ChildItem -Path "${UserDesktopPath}\*" -Include *.lnk -Recurse | Remove-Item;
+  Get-ChildItem -Path "${PublicDesktopPath}\*" -Include *.lnk -Recurse | Remove-Item;
+
+  Write-Host "Shorcuts in desktop successfully deleted." -ForegroundColor "Green";
+}
