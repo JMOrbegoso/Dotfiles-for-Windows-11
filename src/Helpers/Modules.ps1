fe7d0c39d2a1563013c73a4cac43531c6e18fa1a
JMOrbegoso <dev@jmorbegoso.com>
2021-08-07 13:34:48 -0500

    Add Modules.ps1 in Helpers

---
+++ src/Helpers/Modules.ps1
@@ -0,0 +1,38 @@
+function Get-Module-Installation-Status
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Position = 0, Mandatory = $TRUE)]
+    [string]
+    $ModuleName,
+
+    [Parameter(Position = 1, Mandatory = $FALSE)]
+    [string]
+    $ModuleMinimumVersion
+  )
+
+  try
+  {
+    if (-not ([string]::IsNullOrEmpty($ModuleMinimumVersion)))
+    {
+      if ((Get-Module -ListAvailable -Name $ModuleName).Version -ge $ModuleMinimumVersion)
+      {
+        return $TRUE;
+      }
+      return $FALSE;
+    }
+
+    if (Get-Module -ListAvailable -Name $ModuleName)
+    {
+      return $TRUE;
+    }
+    else
+    {
+      return $FALSE;
+    }
+  }
+  catch [Exception]
+  {
+    return $FALSE;
+  }
+}
