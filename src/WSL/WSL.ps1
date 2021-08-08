31a24d5d068e405c45189e3bb6d413a3d4475af3
JMOrbegoso <dev@jmorbegoso.com>
2021-08-08 12:48:51 -0500

    Update WSL.ps1 Install Ubuntu packages.

--- src/WSL/WSL.ps1
+++ src/WSL/WSL.ps1
@@ -1,2 +1,23 @@
 choco install -y "wsl2" --params "/Version:2 /Retry:true";
 choco install -y "wsl-ubuntu-2004" --params "/InstallRoot:true" --execution-timeout 3600;
+
+Write-Host "Updating Ubuntu package repository:" -ForegroundColor "Green";
+wsl sudo apt --yes update;
+
+Write-Host "Upgrading Ubuntu packages:" -ForegroundColor "Green";
+wsl sudo apt --yes upgrade;
+
+Write-Host "Installing CURL in Ubuntu:" -ForegroundColor "Green";
+wsl sudo apt install --yes --no-install-recommends curl;
+
+Write-Host "Installing NeoFetch in Ubuntu:" -ForegroundColor "Green";
+wsl sudo apt install --yes --no-install-recommends neofetch;
+
+Write-Host "Installing Git in Ubuntu:" -ForegroundColor "Green";
+wsl sudo apt install --yes --no-install-recommends git;
+
+Write-Host "Installing Vim in Ubuntu:" -ForegroundColor "Green";
+wsl sudo apt install --yes --no-install-recommends vim;
+
+Write-Host "Installing Zsh in Ubuntu:" -ForegroundColor "Green";
+wsl sudo apt install --yes --no-install-recommends zsh;
