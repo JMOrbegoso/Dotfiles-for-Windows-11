8fcdf95b0c7d8eed087ec8c2455a35f431ab169b
JMOrbegoso <dev@jmorbegoso.com>
2021-08-08 11:38:50 -0500

    Update WSL.ps1 Update Ubuntu 20.04 installation parameters.

--- src/WSL/WSL.ps1
+++ src/WSL/WSL.ps1
@@ -1,2 +1,2 @@
 choco install -y "wsl2" --params "/Version:2 /Retry:true";
-choco install -y "wsl-ubuntu-2004" --execution-timeout 3600;
+choco install -y "wsl-ubuntu-2004" --params "/InstallRoot:true" --execution-timeout 3600;
