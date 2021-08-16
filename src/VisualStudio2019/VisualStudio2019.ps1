8b55473d74be7186ab51a75dcda50d3be6db3216
JMOrbegoso <dev@jmorbegoso.com>
2021-08-16 09:22:32 -0500

    Update VisualStudio2019.ps1 Add function Set-VS2019-Nuget-Configuration.

--- src/VisualStudio2019/VisualStudio2019.ps1
+++ src/VisualStudio2019/VisualStudio2019.ps1
@@ -9,6 +9,20 @@ function Set-OpenWithVSCommunity-To-Extended-ContextMenu {
   }
 }
 
+function Set-VS2019-Nuget-Configuration {
+  $NugetConfigPath = Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath "NuGet" | Join-Path -ChildPath "Config";
+  $DotfilesNugetConfigFile = Join-Path -Path $DotfilesWorkFolder -ChildPath "VisualStudio2019" | Join-Path -ChildPath "Nuget.Config";
+
+  if (-not (Test-Path -Path $NugetConfigPath)) {
+    New-Item $NugetConfigPath -ItemType directory;
+  }
+
+  Write-Host "Configuring NuGet package source:" -ForegroundColor "Green";
+  Copy-Item $DotfilesNugetConfigFile -Destination $NugetConfigPath;
+  Write-Host "NuGet package source successfully configured." -ForegroundColor "Green";
+}
+
 choco install -y "visualstudio2019community" --package-parameters "--passive --locale en-US";
 choco install -y "visualstudio2019-workload-netweb" --package-parameters "--passive --locale en-US" --execution-timeout 7200;
+Set-VS2019-Nuget-Configuration;
 Set-OpenWithVSCommunity-To-Extended-ContextMenu;
