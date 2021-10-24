cc7ce81377451daf15d5f456bf9e7499cb152131
JM Orbegoso <dev@jmorbegoso.com>
2021-10-23 19:33:30 -0500

    Update Windows.ps1 updating power configuration

--- src/Windows/Windows.ps1
+++ src/Windows/Windows.ps1
@@ -94,18 +94,26 @@ function Set-Power-Configuration {
   # AC: Alternating Current (Wall socket).
   # DC: Direct Current (Battery).
 
-  # Turn off disk timeout
+  # Set turn off disk timeout (in minutes / 0: never)
   powercfg -change "disk-timeout-ac" 0;
   powercfg -change "disk-timeout-dc" 0;
-  # Hibernate timeout
+  
+  # Set hibernate timeout (in minutes / 0: never)
   powercfg -change "hibernate-timeout-ac" 0;
   powercfg -change "hibernate-timeout-dc" 0;
-  # Sleep timeout
+
+  # Set sleep timeout (in minutes / 0: never)
   powercfg -change "standby-timeout-ac" 0;
   powercfg -change "standby-timeout-dc" 0;
-  # Turn off screen timeout
-  powercfg -change "monitor-timeout-ac" 5;
-  powercfg -change "monitor-timeout-dc" 5;
+
+  # Set turn off screen timeout (in minutes / 0: never)
+  powercfg -change "monitor-timeout-ac" 10;
+  powercfg -change "monitor-timeout-dc" 10;
+
+  # Set turn off screen timeout on lock screen (in seconds / 0: never)
+  powercfg /SETACVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK 30;
+  powercfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK 30;
+  powercfg /SETACTIVE SCHEME_CURRENT;
 
   Write-Host "Power plan successfully updated." -ForegroundColor "Green";
 }
