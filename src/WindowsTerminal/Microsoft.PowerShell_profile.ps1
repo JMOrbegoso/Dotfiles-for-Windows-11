f2b6626641803b0e9079dfe0934a7440f4275c45
JM Orbegoso <dev@jmorbegoso.com>
2021-11-16 07:12:39 -0500

    Update Microsoft.PowerShell_profile.ps1 adding custom InlinePrediction color for PSReadLine

--- src/WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ src/WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -22,6 +22,10 @@ Set-PSReadlineOption -BellStyle "None";
 Set-PSReadLineOption -PredictionSource "History";
 Set-PSReadLineKeyHandler -Chord "Tab" -Function "MenuComplete";
 
+Set-PSReadLineOption -Colors @{
+  "InlinePrediction" = [ConsoleColor]::DarkGray;
+}
+
 ################################################################################
 #                                  Chocolatey                                  #
 ################################################################################
